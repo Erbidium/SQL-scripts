1679b6cf9ff5bb31cbcc94ac8bcf0700b807cf86
Oleksandr <OlexandrTanasienko@gmail.com>
2021-11-16 19:25:51 +0200

    Add transportations

---
+++ Transportation.sql
@@ -0,0 +1,8 @@
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('10/10/2013', '04/13/2011', 2427, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('10/03/2015', '11/12/2013', 1282, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('09/16/2017', '09/01/2013', 1431, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('07/29/2015', '09/20/2013', 1601, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('11/20/2020', '06/11/2012', 1190, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('12/11/2012', '03/13/2017', 706, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('12/21/2020', '07/07/2020', 2847, 'Baiyang - Baiyang');
+insert into Transportation (DateOfDispatch, DateOfArrival, ActualDistance, FkRouteName) values ('12/15/2013', '09/06/2014', 1008, 'Baiyang - Baiyang');
