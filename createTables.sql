b12049d42599a43239ba1df7357be879b41a192d
Oleksandr <OlexandrTanasienko@gmail.com>
2021-11-16 18:47:30 +0200

    Rework relationships between tables

--- createTables.sql
+++ createTables.sql
@@ -12,21 +12,30 @@ CREATE TABLE Driver(
 	Experience smallint NOT NULL
 	CONSTRAINT "CK_Driver_Experience" CHECK (Experience>=0)
 	CONSTRAINT "DF_Driver_Experience" DEFAULT(0),
-	CategoriesOfLicences varchar(50) NOT NULL,
-	FkPremiumId INT NULL,
-	FkPaymentId INT NOT NULL
+	CategoriesOfLicences varchar(50) NOT NULL
 )
 GO
+
+DROP TABLE Driver
+GO
+
+
 
 CREATE TABLE Route(
 	Name nvarchar(50) NOT NULL PRIMARY KEY,
 	PerfectDistance int NOT NULL
 	CONSTRAINT "CK_Route_Perfect_Distance" CHECK (PerfectDistance>=0)
 	CONSTRAINT "DF_Route_Perfect_Distance" DEFAULT(0),
-	FkTransportationId INT NOT NULL,
-	FkPaymentId INT UNIQUE NOT NULL
+	FixedPriceInUAH money NOT NULL
+	CONSTRAINT "CK_Payment_FixedPriceInUAH" CHECK(FixedPriceInUAH>=0)
+	CONSTRAINT "DF_Payment_FixedPriceInUAH" DEFAULT(0),
 )
 GO
+
+DROP TABLE Route
+GO
+
+
 
 CREATE TABLE Transportation(
 	TransportationId int IDENTITY(1,1) PRIMARY KEY,
@@ -35,9 +44,23 @@ CREATE TABLE Transportation(
 	ActualDistance int NOT NULL 
 	CONSTRAINT "CK_Transportation_Actual_Distance" CHECK(ActualDistance>=0)
 	CONSTRAINT "DF_Transportation_Actual_Distance" DEFAULT(0),
-	FkDriverId INT NOT NULL,
-	FkPremiumId INT NULL
+	FkRouteName nvarchar(50) NOT NULL FOREIGN KEY REFERENCES Route(Name)
 )
+GO
+
+DROP TABLE Transportation
+GO
+
+
+CREATE TABLE TransportationsJournal(
+	TransportationFieldId int IDENTITY(1,1) PRIMARY KEY,
+	TransportationId int NOT NULL FOREIGN KEY REFERENCES Transportation(TransportationId),
+	FkDriverId INT NOT NULL FOREIGN KEY REFERENCES Driver(DriverId),
+	FkPremiumId INT NULL FOREIGN KEY REFERENCES Premium(PremiumId)
+)
+GO
+
+DROP TABLE TransportationsJournal
 GO
 
 CREATE TABLE Premium(
@@ -48,10 +71,5 @@ CREATE TABLE Premium(
 )
 GO
 
-CREATE TABLE Payment(
-	PaymentId int IDENTITY(1,1) PRIMARY KEY,
-	FixedPriceInUAH money NOT NULL
-	CONSTRAINT "CK_Payment_FixedPriceInUAH" CHECK(FixedPriceInUAH>=0)
-	CONSTRAINT "DF_Payment_FixedPriceInUAH" DEFAULT(0)
-)
+DROP TABLE Premium
 GO
