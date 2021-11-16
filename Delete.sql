0a745a200f4f4c813fbbb976029d81a13959dca8
Oleksandr <OlexandrTanasienko@gmail.com>
2021-11-17 00:50:20 +0200

    Add deleting of table contents

---
+++ Delete.sql
@@ -0,0 +1,20 @@
+USE [master]
+GO
+
+USE [FreightTransportationDB]
+GO
+
+DELETE FROM Driver
+GO
+
+DELETE FROM Route
+GO
+
+DELETE From Transportation
+GO
+
+DELETE FROM TransportationsJournal
+GO
+
+DELETE FROM Premium
+GO
